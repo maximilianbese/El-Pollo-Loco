12a54a76b74a4820178c05c65608c4b03c417943
Maximilian Bese <maximilianbese@gmail.com>
2026-03-07 20:41:28 +0100

    Add new status bar images, icons, and initial HTML/CSS/JS setup
    - Added various status bar images for coins, health, and bottles in different colors and states.
    - Included new icons for coins and health indicators.
    - Created an HTML file with a basic structure and linked CSS and JS files.
    - Implemented a JavaScript file to initialize the canvas and load a character image.
    - Added a CSS file for basic styling of the body and canvas.

---
+++ js/game.js
@@ -0,0 +1,9 @@
+let canvas;
+let ctx;
+let character = new Image();
+
+function init() {
+  canvas = document.getElementById("canvas");
+  ctx = ctx = canvas.getContext("2d");
+  character.src = "../img/2_character_pepe/2_walk/W-21.png";
+}
