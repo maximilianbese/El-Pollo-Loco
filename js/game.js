3b44eda052da6a25379312a9f713cf4ec86db314
Maximilian Bese <maximilianbese@gmail.com>
2026-03-09 20:58:58 +0100

    Add character, chicken, and world classes; update game initialization

--- js/game.js
+++ js/game.js
@@ -1,10 +1,10 @@
 let canvas;
 let ctx;
-let character = new MovableObject();
+let world = new World();
 
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
 
-  console.log("My character is", character);
+  console.log("My character is", world.character);
 }
