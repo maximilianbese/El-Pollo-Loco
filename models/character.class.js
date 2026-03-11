2df90e30bea3cb41288fce6514a00cfda85cd57f
Maximilian Bese <maximilianbese@gmail.com>
2026-03-11 22:39:25 +0100

    Refactor BackgroundObject constructor, update Character and Chicken properties, enhance Cloud animation, and modify World class background object initialization

--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,8 @@
 class Character extends MovableObject {
+  height = 280;
+  width = 120;
+  y = 155;
+
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
   }
--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -1,13 +1,24 @@
 class MovableObject {
   x = 120;
-  y = 250;
+  y = 280;
   img;
   height = 150;
   width = 100;
+  imageCache = [];
 
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+  @param {Array} arr - []
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      imageCache[path] = path;
+    });
   }
 
   moveRight() {
