857537584d222ab859a057c5457408e132d13ea5
Maximilian Bese <maximilianbese@gmail.com>
2026-03-26 13:22:38 +0100

    change mobile button and add bottle splash effect

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -7,12 +7,12 @@ class ThrowableObject extends MovableObject {
   ];
 
   IMAGES_SPLASH = [
-    "img/6_salsa_bottle/bottle_splash/1_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_splash/2_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_splash/3_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_splash/4_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_splash/5_bottle_splash.png",
-    "img/6_salsa_bottle/bottle_splash/6_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png",
+    "img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png",
   ];
 
   isSplashing = false;
